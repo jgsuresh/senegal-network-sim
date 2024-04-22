f993abb99efd474d02a995fec54db14b6e889856
Josh Suresh <josh.suresh@gatesfoundation.org>
2024-04-22 15:11:35 -0700

    Transmission sim now has very simple recombination (no crossovers in model; every locus is randomly drawn from parents)

--- network_sim/meiosis_models/super_simple.py
+++ network_sim/meiosis_models/super_simple.py
@@ -24,7 +24,7 @@ def meiosis(parent1_genotype, parent2_genotype):
         np.random.shuffle(offspring_genotypes_matrix[:, col])
 
     # Split the matrix back into 4 separate genotypes
-    offspring_genotypes = np.vsplit(offspring_genotypes_matrix, 4)
+    offspring_genotypes = [row[0] for row in np.vsplit(offspring_genotypes_matrix, 4)]
     return offspring_genotypes
 
 def num_oocysts(model="fpg", min_oocysts=0):
@@ -41,9 +41,10 @@ def num_oocysts(model="fpg", min_oocysts=0):
     else:
         raise ValueError("Model not recognized.")
 
-def get_oocyst_genotypes(gametocyte_genotypes, gametocyte_densities=None, num_oocyst_function=None):
+def gametocyte_to_oocyst_offspring_genotypes(gametocyte_genotypes, gametocyte_densities=None, num_oocyst_model="fpg"):
     # Assumes gametocytype genotypes is a list of arrays. Each element of the list is a different genotype.
-    # Assumes all oocyst offspring have equal likelihood to be onwardly transmitted
+    # Assumes all oocyst offspring have equal likelihood to be onwardly transmitted.
+    # Note that in the case of selfing, all four offspring genotypes are passed on, to account for higher likelihood of onward transmission.
     #todo Add root tracking
 
     if gametocyte_densities is not None:
@@ -56,7 +57,7 @@ def get_oocyst_genotypes(gametocyte_genotypes, gametocyte_densities=None, num_oo
         offspring_genotypes = []
 
         # Compute number of oocysts
-        n_oocyst = num_oocysts(model="fpg", min_oocysts=1)
+        n_oocyst = num_oocysts(model=num_oocyst_model, min_oocysts=1)
 
         # For each oocyst, choose two gametocyte genomes at random to mate:
         for i in range(n_oocyst):
@@ -66,29 +67,98 @@ def get_oocyst_genotypes(gametocyte_genotypes, gametocyte_densities=None, num_oo
         return offspring_genotypes
 
 
+def num_sporozites(model="fpg", min_sporozoites=0):
+    if model=="fpg":
+        # Parameters
+        r = 12  # number of failures. EMOD param Num_Sporozoite_In_Bite_Fail
+        p = 0.5  # probability of failure. EMOD param Probability_Sporozoite_In_Bite_Fails
+
+        return np.max([min_sporozoites, np.random.negative_binomial(r, p)])
+
+    else:
+        raise ValueError("Model not recognized.")
+
+def oocyst_offspring_to_sporozoite_genotypes(oocyst_offspring_genotypes):
+    n_spz = num_sporozites(model="fpg", min_sporozoites=1)
+    return random.choices(oocyst_offspring_genotypes, k=n_spz)
+
+def gametocyte_to_sporozoite_genotypes(gametocyte_genotypes, gametocyte_densities=None):
+    contains_nan = any(np.isnan(element).any() for element in gametocyte_genotypes)
+    if contains_nan:
+        raise ValueError("NaNs found in gametocyte genotypes.")
 
 
-if False:
-    # Test the function
-    parent1_genotype = np.array([0, 1, 1, 1, 0, 1, 0, 1, 0, 1])
-    parent2_genotype = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
-    offspring_genotypes = meiosis(parent1_genotype, parent2_genotype)
-    print(offspring_genotypes)
+    oocyst_offspring_genotypes = gametocyte_to_oocyst_offspring_genotypes(gametocyte_genotypes, gametocyte_densities)
+    sporozoite_genotypes = oocyst_offspring_to_sporozoite_genotypes(oocyst_offspring_genotypes)
 
-if False:
-    # Test the function
-    import pandas as pd
-    N_initial_infections = 10
-    N_barcode_positions = 15
+    # Remove duplicates - #fixme Account for different likelihoods of onward transmission
+    if len(sporozoite_genotypes) == 1:
+        return sporozoite_genotypes
+    else:
+        sporozoite_genotypes_without_duplicates = np.unique(np.vstack(sporozoite_genotypes), axis=0)
+        sporozoite_genotypes_without_duplicates = [row[0] for row in np.vsplit(sporozoite_genotypes_without_duplicates, sporozoite_genotypes_without_duplicates.shape[0])]
+        return sporozoite_genotypes_without_duplicates
 
-    genotype_lookup = pd.DataFrame({"genotype_id": np.arange(N_initial_infections),
-                                    "infection_id": np.arange(N_initial_infections)})
+def _explore_sporozoite_diversity(n_unique_gametocyte_genotypes=10, n_barcode_positions=15):
+    # Compute number of unique genotypes in sporozoites
 
-    # Initialize actual genotypes based on allele frequencies.
-    # Set columns for each of N_barcode_positions
-    for i in range(1, N_barcode_positions + 1):
-        genotype_lookup[f"pos_{str(i).zfill(3)}"] = np.random.binomial(n=1, p=0.5, size=N_initial_infections)
+    # Initialize gametocyte genotypes randomly
+    gametocyte_genotypes = []
+    for i in range(1, n_unique_gametocyte_genotypes + 1):
+        gametocyte_genotypes.append(np.random.binomial(n=1, p=0.5, size=n_barcode_positions))
 
-    print(genotype_lookup)
+    oocyst_offspring_genotypes = gametocyte_to_oocyst_offspring_genotypes(gametocyte_genotypes)
+    sporozoite_genotypes = oocyst_offspring_to_sporozoite_genotypes(oocyst_offspring_genotypes)
 
-    get_oocyst_genotypes(genotype_lookup.iloc[:, 2:].values)
+    # Count the number of unique genotypes in oocyst offspring
+    unique_oocyst_genotypes = np.unique(np.array(oocyst_offspring_genotypes), axis=0)
+    n_unique_oocyst_genotypes = unique_oocyst_genotypes.shape[0]
+    # print(f"Number of unique oocyst genotypes: {n_unique_oocyst_genotypes}")
+
+    # Count the number of unique genotypes in sporozoites
+    unique_sporozoite_genotypes = np.unique(np.array(sporozoite_genotypes), axis=0)
+    n_unique_sporozoite_genotypes = unique_sporozoite_genotypes.shape[0]
+    # print(f"Number of unique sporo genotypes: {n_unique_sporozoite_genotypes}")
+    return n_unique_oocyst_genotypes, n_unique_sporozoite_genotypes
+
+
+if __name__ == "__main__":
+    if False:
+        # Test the function
+        parent1_genotype = np.array([0, 1, 1, 1, 0, 1, 0, 1, 0, 1])
+        parent2_genotype = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+        offspring_genotypes = meiosis(parent1_genotype, parent2_genotype)
+        print(offspring_genotypes)
+
+    if False:
+        # Test the function
+        import pandas as pd
+        N_initial_infections = 10
+        N_barcode_positions = 15
+
+        genotype_lookup = pd.DataFrame({"genotype_id": np.arange(N_initial_infections),
+                                        "infection_id": np.arange(N_initial_infections)})
+
+        # Initialize actual genotypes based on allele frequencies.
+        # Set columns for each of N_barcode_positions
+        for i in range(1, N_barcode_positions + 1):
+            genotype_lookup[f"pos_{str(i).zfill(3)}"] = np.random.binomial(n=1, p=0.5, size=N_initial_infections)
+
+        print(genotype_lookup)
+
+        get_oocyst_genotypes(genotype_lookup.iloc[:, 2:].values)
+
+    a = np.zeros(10000)
+    b = np.zeros(10000)
+    for i in range(10000):
+        a[i], b[i] = _explore_sporozoite_diversity(n_unique_gametocyte_genotypes=5, n_barcode_positions=150)
+
+    import matplotlib.pyplot as plt
+    plt.hist(a, bins=range(1,50), label="offspring")
+    plt.hist(b, bins=range(1, 50), label="sporozoites", histtype="step")
+    plt.axvline(np.mean(a))
+    plt.axvline(np.mean(b))
+    plt.legend()
+    print(np.mean(a))
+    print(np.mean(b))
+    plt.show()
--- network_sim/sim.py
+++ network_sim/sim.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import cProfile
 
+from network_sim.meiosis_models.super_simple import gametocyte_to_sporozoite_genotypes
 
 
 def human_to_vector_transmission(infection_lookup, vector_lookup, human_lookup):
@@ -17,7 +18,8 @@ def human_to_vector_transmission(infection_lookup, vector_lookup, human_lookup):
     else:
         # Append these mosquitos to latent mosquito array
         vector_ids = np.arange(total_num_surviving_mosquitos)
-        infection_ids_picked_up = np.repeat(human_lookup["infection_ids"].values, n_surviving_mosquitos)
+        # acquired_infection_ids = np.repeat(human_lookup["infection_ids"].values, n_surviving_mosquitos)
+        gametocyte_genotypes = np.repeat(human_lookup["infection_genotypes"].values, n_surviving_mosquitos)
         days_until_next_bite = np.ones_like(vector_ids) * (11 + 1)
 
         if bites_from_infected_mosquito_distribution == "constant":
@@ -28,10 +30,18 @@ def human_to_vector_transmission(infection_lookup, vector_lookup, human_lookup):
 
         new_vector_lookup = pd.DataFrame({
             "vector_id": vector_ids,
-            "infection_ids_picked_up": infection_ids_picked_up,
+            # "acquired_infection_ids": acquired_infection_ids,
+            "gametocyte_genotypes": gametocyte_genotypes,
             "days_until_next_bite": days_until_next_bite,
             "total_bites_remaining": total_bites_remaining
         })
+
+        # vector_lookup["gametocyte_genotypes"] is a list of arrays. Check that no array is actually a nan
+        contains_nan = new_vector_lookup["gametocyte_genotypes"].apply(lambda x: np.isnan(x).any()).any()
+        if contains_nan:
+            raise ValueError("NaNs found in gametocyte genotypes.")
+
+        new_vector_lookup = determine_sporozoite_genotypes(new_vector_lookup)
 
         if vector_lookup.shape[0] == 0:
             return new_vector_lookup
@@ -42,11 +52,13 @@ def human_to_vector_transmission(infection_lookup, vector_lookup, human_lookup):
 
 def vector_to_human_transmission(infection_lookup, vector_lookup, human_lookup):
     if vector_lookup.shape[0] == 0:
+        # Only need to do this if there are vectors at all
         return infection_lookup, vector_lookup, human_lookup
     else:
-        indices = vector_lookup["days_until_next_bite"] == 0
+        # Determine which vectors are ready to bite today
+        vectors_biting_today = vector_lookup[vector_lookup["days_until_next_bite"] == 0]
+        n_new_infectious_bites = vectors_biting_today.shape[0]
 
-        n_new_infectious_bites = indices.sum()
         # If no vectors are ready to bite, return the infection lookup unchanged
         if n_new_infectious_bites == 0:
             return infection_lookup, vector_lookup, human_lookup
@@ -59,18 +71,29 @@ def vector_to_human_transmission(infection_lookup, vector_lookup, human_lookup):
             # New infectious bites delivered proportionally based on bite rate
             weights = human_lookup["daily_bite_rate"]/human_lookup["daily_bite_rate"].sum()
 
-            if infectiousness_distribution == "constant":
-                infectiousness = np.ones(n_new_infectious_bites) * individual_infectiousness
-            elif infectiousness_distribution == "exponential":
-                infectiousness = np.random.exponential(scale=individual_infectiousness, size=n_new_infectious_bites)
+            # Holder containing sporozoite genomes and human ids of people newly infected
+            new_infections = vectors_biting_today.copy()
+            new_infections["human_id"] = np.random.choice(human_ids, size=n_new_infectious_bites, p=weights, replace=True)
 
-            new_infections = pd.DataFrame({
-                "infection_id": max_infection_id + 1 + np.arange(n_new_infectious_bites), #fixme for now, may repeat infection ids
-                # "human_id": np.random.choice(human_ids, n_new_infectious_bites, replace=True), #missing biting heterogeneity, max num infections
-                "human_id": np.random.choice(human_ids, size=n_new_infectious_bites, p=weights, replace=True), #fixme max num infections
-                "infectiousness": infectiousness, #fixme draw from distribution
-                "days_until_clearance": individual_infection_duration + 1 #fixme draw from distribution
-            })
+            # The "sporozoite_genotypes" column of new_infection_hold is a list of genotypes.
+            # Create a new dataframe which has a single row for each sporozoite genotype
+            new_infections = new_infections.explode("sporozoite_genotypes")
+
+            # Rename columns and keep only relevant ones
+            new_infections = (new_infections[["human_id", "sporozoite_genotypes"]].
+                              rename(columns={"sporozoite_genotypes": "genotype"}))
+
+            # Add infectiousness information
+            if infectiousness_distribution == "constant":
+                infectiousness = np.ones(new_infections.shape[0]) * individual_infectiousness
+            elif infectiousness_distribution == "exponential":
+                infectiousness = np.random.exponential(scale=individual_infectiousness, size=new_infections.shape[0])
+            else:
+                raise ValueError("Invalid infectiousness distribution")
+            new_infections["infectiousness"] = infectiousness
+
+            # Add infection duration information
+            new_infections["days_until_clearance"] = individual_infection_duration + 1 #fixme draw from distribution
 
             # Append new infections to infection lookup
             infection_lookup = pd.concat([infection_lookup, new_infections], ignore_index=True)
@@ -121,25 +144,10 @@ def generate_human_lookup_from_infection_lookup(infection_lookup, human_lookup=N
 
     # human_lookup["infection_ids"] = [list(infection_lookup[infection_lookup["human_id"] == human_id]["infection_id"]) for human_id in human_ids] #fixme may be able to optimize this
     # Group the infection_lookup DataFrame by 'human_id' and aggregate 'infection_id' into lists
-    grouped = infection_lookup.groupby('human_id')['infection_id'].apply(list)
-
-    # Assign the result to the 'infection_ids' column in human_lookup
-    human_lookup['infection_ids'] = human_lookup['human_id'].map(grouped)
-
-    # Fill NaN values with empty lists for humans with no infections
-    # human_lookup['infection_ids'].fillna(value=[], inplace=True)
-    # human_lookup['infection_ids'].fillna(value=[[] for _ in range(len(human_lookup))], inplace=True)
-
-    # human_lookup["is_infected"] = [len(infection_ids) > 0 for infection_ids in human_lookup["infection_ids"]]
+    human_lookup['infection_ids'] = human_lookup['human_id'].map(infection_lookup.groupby('human_id')['infection_id'].apply(list))
+    human_lookup['infection_genotypes'] = human_lookup['human_id'].map(infection_lookup.groupby('human_id')['genotype'].apply(list))
     human_lookup["is_infected"] = np.logical_not(human_lookup["infection_ids"].isna())
-    # human_lookup["infectiousness"] = [infection_lookup[infection_lookup["human_id"] == human_id]["infectiousness"].max() for human_id in human_ids]
-    # human_lookup["infectiousness"].fillna(0, inplace=True)
-    # Group the infection_lookup DataFrame by 'human_id' and get the max 'infectiousness'
-    # grouped = infection_lookup.groupby('human_id')['infectiousness'].max()
-    grouped = infection_lookup.groupby('human_id')['infectiousness'].sum()
-
-    # Assign the result to the 'infectiousness' column in human_lookup
-    human_lookup['infectiousness'] = human_lookup['human_id'].map(grouped)
+    human_lookup['infectiousness'] = human_lookup['human_id'].map(infection_lookup.groupby('human_id')['infectiousness'].max())
 
     # Fill NaN values with 0 for humans with no infections
     human_lookup['infectiousness'].fillna(0, inplace=True)
@@ -162,32 +170,40 @@ def initialize_genotype_lookup():
 
     return genotype_lookup
 
-def recombine_in_vectors(vector_lookup):
-    # Get vectors with multiple infections
-    has_multiple_infections = vector_lookup["infection_id"].apply(lambda x: len(x) > 1)
 
-    # For each vector with multiple infections, simulate recombination
-    # for i in range(vector_lookup[has_multiple_infections].shape[0]):
-    #     vector = vector_lookup[has_multiple_infections].iloc[i]
-    #     strain1 = vector["infection_id"].iloc[0]
-    #     strain2 = vector["infection_id"].iloc[1]
-    #     strain1_genotype = strain_lookup[strain_lookup["strain_id"] == strain1]
-    #     strain2_genotype = strain_lookup[strain_lookup["strain_id"] == strain2]
-    #
-    #     # Recombine genotypes
-    #     new_genotype = strain1_genotype.copy()
-    #     for i in range(1, N_barcode_positions+1):
-    #         if np.random.binomial(n=1, p=0.5):
-    #             new_genotype[f"pos_{str(i).zfill(3)}"] = strain1_genotype[f"pos_{str(i).zfill(3)}"]
-    #         else:
-    #             new_genotype[f"pos_{str(i).zfill(3)}"] = strain2_genotype[f"pos_{str(i).zfill(3)}"]
-    #
-    #     # Add new genotype to strain lookup
-    #     strain_lookup = pd.concat([strain_lookup, new_genotype], ignore_index=True)
-    #     new_strain_id = strain_lookup["strain_id"].max()
-    #
-    #     # Update vector lookup to reflect recombination
-    #     vector_lookup.loc[vector_lookup["vector_id"] == vector["vector_id"], "infection_id"] = new_strain_id
+def determine_sporozoite_genotypes(vector_lookup):
+    # Determine sporozoite genotypes (i.e. the genotypes that each vector will transmit)
+
+    # vector_lookup["gametocyte_genotypes"] is a list of arrays. Check that no array is actually a nan
+    contains_nan = vector_lookup["gametocyte_genotypes"].apply(lambda x: np.isnan(x).any()).any()
+    if contains_nan:
+        raise ValueError("NaNs found in gametocyte genotypes.")
+
+    # If vector has a single infection, then the transmitting genotype is the same as the infection genotype
+    no_recombination_needed = vector_lookup["gametocyte_genotypes"].apply(lambda x: len(x) == 1)
+    if no_recombination_needed.sum() > 0:
+        vector_lookup.loc[no_recombination_needed, "sporozoite_genotypes"] = vector_lookup.loc[no_recombination_needed, "gametocyte_genotypes"]
+        # vector_lookup.loc[has_single_infection_objects, "transmitting_infection_ids"] = vector_lookup.loc[has_single_infection_objects, "acquired_infection_ids"]
+
+    # If vector has multiple infections, then simulate recombination
+    has_multiple_infection_objects = vector_lookup["gametocyte_genotypes"].apply(lambda x: len(x) > 1)
+    if has_multiple_infection_objects.sum() > 0:
+        # Get the rows with multiple infection objects
+        multiple_infection_rows = vector_lookup[has_multiple_infection_objects]
+
+        # Calculate the sporozoite genotypes for these rows
+        sporozoite_genotypes = multiple_infection_rows["gametocyte_genotypes"].apply(gametocyte_to_sporozoite_genotypes)
+
+        # Update the sporozoite genotypes in the original DataFrame
+        vector_lookup.loc[has_multiple_infection_objects, "sporozoite_genotypes"] = sporozoite_genotypes
+    return vector_lookup
+
+
+def get_max_infection_id(infection_lookup):
+    if infection_lookup.shape[0] == 0:
+        return 0
+    else:
+        return infection_lookup["infection_id"].max()
 
 
 
@@ -203,7 +219,7 @@ daily_bite_rate_distribution = "constant" # "exponential"
 prob_survive_to_infectiousness = 1 # 0.36
 bites_from_infected_mosquito_distribution = "constant"
 mean_bites_from_infected_mosquito = 1 # 1.34
-N_barcode_positions = 5
+N_barcode_positions = 24
 
 human_ids = np.arange(N_individuals)
 # emod_lifespans = np.ones(100)
@@ -224,39 +240,46 @@ def main():
         infectiousness = np.random.exponential(scale=individual_infectiousness, size=N_initial_infections)
     else:
         raise ValueError("Invalid infectiousness distribution")
-    infection_duration = np.ones(N_initial_infections) * individual_infection_duration
 
     # Distribute initial infections randomly to humans, with random time until clearance
     indices = np.random.choice(human_ids, N_initial_infections, replace=True)
-    infection_lookup = pd.DataFrame({"infection_id": infection_ids,
-                                     "human_id": indices,
-                                     "infectiousness": infectiousness,
-                                     "days_until_clearance": np.random.randint(1, individual_infection_duration+1, N_initial_infections)})
+    human_infection_lookup = pd.DataFrame({"infection_id": infection_ids,
+                                           "human_id": indices,
+                                           "infectiousness": infectiousness,
+                                           "days_until_clearance": np.random.randint(1, individual_infection_duration+1, N_initial_infections)})
+
+    all_genotype_matrix = np.random.binomial(n=1, p=0.5, size=(N_initial_infections, N_barcode_positions)) #fixme Allow for locus-specific allele frequencies
+    human_infection_lookup["genotype"] = [row[0] for row in np.vsplit(all_genotype_matrix, N_initial_infections)]
 
     # Generate human and vector lookups
-    human_lookup = generate_human_lookup_from_infection_lookup(infection_lookup)
-    vector_lookup = pd.DataFrame({"vector_id": [], "infection_id": [], "days_until_next_bite": []})
-
-    strain_lookup = initialize_strain_lookup()
+    human_lookup = generate_human_lookup_from_infection_lookup(human_infection_lookup)
+    vector_lookup = pd.DataFrame({"vector_id": [],
+                                  # "acquired_infection_ids": [],
+                                  "gametocyte_genotypes": [],
+                                  "sporozoite_genotypes": [],
+                                  "days_until_next_bite": []})
 
     # Set up summary statistics
     summary_statistics = pd.DataFrame({"time": [],
                                        "n_infections": [],
                                        "n_humans_infected": [],
-                                       "n_infected_vectors": []})
+                                       "n_infected_vectors": [],
+                                       # "n_unique_strains": []
+                                       })
 
     # Loop over timesteps
     for t in range(sim_duration):
-        vector_lookup = human_to_vector_transmission(infection_lookup, vector_lookup, human_lookup)
-        infection_lookup, vector_lookup, human_lookup = vector_to_human_transmission(infection_lookup, vector_lookup, human_lookup)
+        # Simulate human to vector transmission
+        vector_lookup = human_to_vector_transmission(human_infection_lookup, vector_lookup, human_lookup)
+        human_infection_lookup, vector_lookup, human_lookup = vector_to_human_transmission(human_infection_lookup, vector_lookup, human_lookup)
 
         # Timestep bookkeeping: clear infections which have completed their duration, update vector clocks
-        infection_lookup, vector_lookup = timestep_bookkeeping(infection_lookup, vector_lookup)
+        human_infection_lookup, vector_lookup = timestep_bookkeeping(human_infection_lookup, vector_lookup)
 
         # Record summary statistics
         summary_statistics = pd.concat([summary_statistics,
                                         pd.DataFrame({"time": [t],
-                                                      "n_infections": [infection_lookup.shape[0]],
+                                                      "n_infections": [human_infection_lookup.shape[0]],
                                                       "n_humans_infected": [human_lookup["is_infected"].sum()],
                                                       "n_infected_vectors": [vector_lookup.shape[0]]})], ignore_index=True)
 
@@ -268,6 +291,11 @@ def main():
     plt.plot(summary_statistics["time"], summary_statistics["n_infected_vectors"], label="Number of vectors")
     plt.legend()
     plt.show()
+
+    # Save final state
+    human_lookup.to_csv("human_lookup.csv", index=False)
+    vector_lookup.to_csv("vector_lookup.csv", index=False)
+    human_infection_lookup.to_csv("human_infection_lookup.csv", index=False)
 
 if __name__ == "__main__":
     cProfile.run('main()')
